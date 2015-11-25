acb8c9ab132a1f8d2719326aa9571b6bdbf23470
metodiew <stanko@metodiew.com>
2015-11-25 17:54:27 +0200

    wp-uploads-backup.sh - add scp to the server script

--- wp-uploads-backup.sh
+++ wp-uploads-backup.sh
@@ -5,6 +5,8 @@
 today="$(date +'%Y.%m.%d')"
 directory="/var/www/html/wp-content/uploads/"
 
+
 if [ -d "$directory" ]; then
   zip -r uploads-$today.zip $directory
+  sshpass -p 'server-pass-here' scp uploads-$today.zip server-user-here@XX.XX.XX.XX:/home/user/backup/
 fi
